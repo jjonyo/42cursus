24fd524973158eced4e412ce40edaf54c5abecf0
jjonyo <jonghyuk96@cau.ac.kr>
2021-06-26 13:56:02 +0900

    ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jonghpar <jonghpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/26 13:52:51 by jonghpar          #+#    #+#             */
+/*   Updated: 2021/06/26 13:55:40 by jonghpar         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	char	*ret;
+
+	if (!s || !f)
+		return (NULL);
+	if (!(ret = (char *)malloc(ft_strlen(s) + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ret[i] = f(i, s[i]);
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
