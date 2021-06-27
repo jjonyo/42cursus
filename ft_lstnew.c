3ac8ffea2aebd32e9d8fbfd1f9f0adaf78029b41
jjonyo <jonghyuk96@cau.ac.kr>
2021-06-27 23:57:07 +0900

    .

---
+++ ft_lstnew.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jonghpar <jonghpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/27 23:56:01 by jonghpar          #+#    #+#             */
+/*   Updated: 2021/06/27 23:56:56 by jonghpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *new;
+
+	new = (t_list*)malloc(sizeof(t_list));
+	new->content = content;
+	return (new);
+}
