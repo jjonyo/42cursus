de0d011f426e85fcfbe7167f7c5005fe299edc1c
jjonyo <jonghyuk96@cau.ac.kr>
2021-07-07 00:35:00 +0900

    gnl

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jonghpar <jonghpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/03 10:47:35 by jonghpar          #+#    #+#             */
+/*   Updated: 2021/07/06 21:40:03 by jonghpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+
+# define SUCCESS 1
+# define END 0
+# define ERROR -1
+# define TRUE 1
+# define FALSE 0
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+int		ft_strlcpy(char *dest, const char *src, size_t d_size);
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *str);
+int		ft_strchr(const char *str, int c);
+#endif
