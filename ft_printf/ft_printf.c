74f291742b38dce5c2253ef4f81b36a88d7c0bd9
jjonyo <jonghyuk96@cau.ac.kr>
2021-12-21 00:06:27 +0900

    [ft_printf] Norminette

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,48 +6,68 @@
 /*   By: jonghpar <student.42seoul.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 13:06:58 by jonghpar          #+#    #+#             */
-/*   Updated: 2021/12/20 22:39:51 by jonghpar         ###   ########.fr       */
+/*   Updated: 2021/12/21 00:00:24 by jonghpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_parse(va_list ap, const char c)
+int	ft_putchar(int c)
 {
-    if (c == 'd' || c == 'i')
-        return (ft_putnbr(va_arg(ap, int)));
-    else if (c == 'c')
-        return (ft_putchar(va_arg(ap, int)));
-    else if (c == 's')
-        return (ft_putstr(va_arg(ap, char *)));
-    else if (c == 'p')
-        return (ft_putptr(va_arg(ap, void *))); //이부분 안되면 unsigned long 으로 바꾸기
-    else if (c == 'u')
-        return (ft_put_unsigned_nbr(va_arg(ap, int)));
-    else if (c == 'X')
-        return (ft_puthex(va_arg(ap, int), TRUE));
-    else if (c == 'x')
-        return (ft_puthex(va_arg(ap, int), FALSE));
-    else 
-        return (ft_putchar(c));
+	write(1, &c, 1);
+	return (1);
 }
 
-int ft_printf(const char *fmt, ...)
+int	ft_putstr(char *str)
 {
-    va_list ap;
-    int i;
-    int ret;
+	int	i;
+	int	len;
 
-    i = -1;
-    ret = 0;
-    va_start(ap, fmt);
-    while(fmt[++i])
-    {
-        if (fmt[i] == '%')
-            ret += ft_parse(ap, fmt[++i]);
-        else
-            ret += ft_putchar(fmt[i]);
-    }
-    va_end(ap);
-    return (ret);
+	i = -1;
+	len = 0;
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+	while (str[++i])
+		write(1, &str[i], 1);
+	return (i);
+}
+
+int	ft_parse(va_list ap, const char c)
+{
+	if (c == 'd' || c == 'i')
+		return (ft_putnbr(va_arg(ap, int)));
+	else if (c == 'c')
+		return (ft_putchar(va_arg(ap, int)));
+	else if (c == 's')
+		return (ft_putstr(va_arg(ap, char *)));
+	else if (c == 'p')
+		return (ft_putptr(va_arg(ap, void *)));
+	else if (c == 'u')
+		return (ft_put_unsigned_nbr(va_arg(ap, int)));
+	else if (c == 'X')
+		return (ft_puthex(va_arg(ap, int), TRUE));
+	else if (c == 'x')
+		return (ft_puthex(va_arg(ap, int), FALSE));
+	else
+		return (ft_putchar(c));
+}
+
+int	ft_printf(const char *fmt, ...)
+{
+	va_list	ap;
+	int		i;
+	int		ret;
+
+	i = -1;
+	ret = 0;
+	va_start(ap, fmt);
+	while (fmt[++i])
+	{
+		if (fmt[i] == '%')
+			ret += ft_parse(ap, fmt[++i]);
+		else
+			ret += ft_putchar(fmt[i]);
+	}
+	va_end(ap);
+	return (ret);
 }
--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,86 +6,66 @@
 /*   By: jonghpar <student.42seoul.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 18:51:02 by jonghpar          #+#    #+#             */
-/*   Updated: 2021/12/20 23:28:33 by jonghpar         ###   ########.fr       */
+/*   Updated: 2021/12/21 00:03:57 by jonghpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putchar(int c)
+int	ft_puthex(unsigned int n, int is_upper)
 {
-    write(1, &c, 1);
-    return (1);
+	char	*hex;
+	int		len;
+
+	len = 0;
+	if (is_upper)
+		hex = "0123456789ABCDEF";
+	else
+		hex = "0123456789abcdef";
+	if (n > 15)
+		len = ft_puthex(n / 16, is_upper);
+	len += ft_putchar(hex[n % 16]);
+	return (len);
 }
 
-int ft_puthex(unsigned int n, int is_upper)
+int	ft_ptr_to_hex(unsigned long n)
 {
-    char    *hex;
-    int     len;
+	char	*hex;
+	int		len;
 
-    len = 0;
-    if (is_upper)
-        hex = "0123456789ABCDEF";
-    else
-        hex = "0123456789abcdef";
-    if (n > 15)
-        len = ft_puthex(n / 16, is_upper);
-    len += ft_putchar(hex[n % 16]);
-    return (len);
+	len = 0;
+	hex = "0123456789abcdef";
+	if (n > 15)
+		len = ft_ptr_to_hex(n / 16);
+	len += ft_putchar(hex[n % 16]);
+	return (len);
 }
 
-int ft_ptr_to_hex(unsigned long n)
+int	ft_putptr(void *ptr)
 {
-    char    *hex;
-    int     len;
+	unsigned long	n;
 
-    len = 0;
-    hex = "0123456789abcdef";
-    if (n > 15)
-        len = ft_ptr_to_hex(n / 16);
-    len += ft_putchar(hex[n % 16]);
-    return (len);
+	n = (unsigned long)ptr;
+	return (ft_putstr("0x") + ft_ptr_to_hex(n));
 }
 
-int ft_putptr(void *ptr)
+int	ft_put_unsigned_nbr(unsigned int n)
 {
-    unsigned long   n;
+	int	len;
 
-    n = (unsigned long)ptr;
-    return (ft_putstr("0x") + ft_ptr_to_hex(n));
+	len = 0;
+	if (n >= 10)
+		len = ft_put_unsigned_nbr(n / 10);
+	len += ft_putchar(n % 10 + '0');
+	return (len);
 }
 
-int ft_putstr(char *str)
+int	ft_putnbr(int n)
 {
-    int i;
-    int len;
+	int	len;
 
-    i = -1;
-    len = 0;
-    if (str == NULL)
-        return (write(1, "(null)", 6));
-    while (str[++i])
-        write(1, &str[i], 1);
-    return (i);
-}
-
-int ft_put_unsigned_nbr(unsigned int n)
-{
-    int len;
-
-    len = 0;
-    if (n >= 10)
-        len = ft_put_unsigned_nbr(n / 10);
-    len += ft_putchar(n % 10 + '0');
-    return (len);
-}
-
-int ft_putnbr(int n)
-{
-    int len;
-
-    len = 0;
-    if (n == -2147483648)
+	len = 0;
+	if (n == -2147483648)
 		return (ft_putstr("-2147483648"));
 	else if (n < 0)
 	{
@@ -98,6 +78,5 @@ int ft_putnbr(int n)
 			len = ft_putnbr(n / 10);
 		len += ft_putchar(n % 10 + '0');
 	}
-    return (len);
+	return (len);
 }
-
