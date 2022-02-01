e493fc8d97122e7bb0cbc8c5585f99934d8acd56
jjonyo <jonghyuk96@cau.ac.kr>
2022-02-01 18:39:20 +0900

    [pipex]

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,54 +6,11 @@
 /*   By: jonghpar <student.42seoul.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:54:07 by jonghpar          #+#    #+#             */
-/*   Updated: 2022/02/01 14:43:11 by jonghpar         ###   ########.fr       */
+/*   Updated: 2022/02/01 18:38:59 by jonghpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
-
-void	error(void)
-{
-	perror("\033[31mError: ");
-	exit(1);
-}
-
-char	*find_path(char *command, char **envp)
-{
-	char	**paths;
-	char	*path;
-	char	*part_path;
-	int		i;
-
-	i = 0;
-	while (ft_strnstr(envp[i], "PATH", 4) == 0)
-		i++;
-	paths = ft_split(envp[i] + 5, ':');
-	i = 0;
-	while (paths[i])
-	{
-		part_path = ft_strjoin(paths[i], "/");
-		path = ft_strjoin(part_path, command);
-		free(part_path);
-		if (access(path, F_OK) == 0)
-			return (path);
-		i++;
-	}
-	return (0);
-}
-
-void	execute(char *argv, char **envp)
-{
-	char	*path;
-	char	**command;
-
-	command = ft_split(argv, ' ');
-	path = find_path(command[0], envp);
-	if (!path)
-		error();
-	if (execve(path, command, envp) == -1)
-		error();
-}
 
 void	child_process(int *fd, char **argv, char **envp)
 {
